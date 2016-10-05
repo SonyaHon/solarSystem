259538eae252b8f10ede2cb7b26fad9057b03fac
SonyaHon <volkov030@gmail.com>
2016-10-05 22:57:52 +0400

    added some planets

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
 var SCENE, CAMERA, RENDER; // for render and cam
 var OBJECTS = []; // all scene objects;
 var EARTH_SIZE = 1;
-var KILLOMETR = 0.0001;
-var ASTRO_ONE = 100;
-
-
+var ASTRO_ONE = 100; // Коэфицент перехода к нормальной астрономической единице = 1498е+6
+var EARTH_YEAR = 0.0007;
 
 function init() {
 	var container = document.createElement('div');
@@ -13,7 +11,7 @@ function init() {
 	CAMERA = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 10000);
 	
 	CAMERA.position.y = 0;
-	CAMERA.position.z = 70;
+	CAMERA.position.z = 200;
 
 	SCENE = new THREE.Scene();
 
@@ -27,17 +25,62 @@ function init() {
 
 function initObjs() {
 
+	var sun = new spaceObj({texture: "imgs/texture_sun.jpg", size: 109 * EARTH_SIZE / 3});
+	OBJECTS['sun'] = sun;
+	SCENE.add(OBJECTS['sun'].getMesh());
+
+	var mercury = new spaceObj({texture: "imgs/texture_mercury.jpg", size: EARTH_SIZE / 3, x: 0.5*ASTRO_ONE,
+	orbitRotationSpeed: EARTH_YEAR * 4});
+	OBJECTS['mercury'] = mercury;
+	SCENE.add(OBJECTS['mercury'].getMesh());
+
+	var venus = new spaceObj({texture: "imgs/texture_venus.jpg", size: EARTH_SIZE * 0.95, x: ASTRO_ONE*0.75,
+	orbitRotationSpeed: EARTH_YEAR * 1.05});
+	OBJECTS['venus'] = venus;
+	SCENE.add(OBJECTS['venus'].getMesh());
+
 	var earth = new spaceObj({texture: "imgs/texture_earth.jpg", size: EARTH_SIZE, x: ASTRO_ONE});
 	OBJECTS['earth'] = earth;
 	SCENE.add(OBJECTS['earth'].getMesh());
 
-	var sun = new spaceObj({texture: "imgs/texture_sun.jpg", size: 109 * EARTH_SIZE / 3});
-	OBJECTS['sun'] = sun;
-	SCENE.add(OBJECTS['sun'].getMesh());
+	var mars = new spaceObj({texture: "imgs/texture_mars.jpg", size: EARTH_SIZE * 0.5, x: ASTRO_ONE*1.4,
+		orbitRotationSpeed: EARTH_YEAR * 0.55});
+	OBJECTS['mars'] = mars;
+	SCENE.add(OBJECTS['mars'].getMesh());
+
+	var jupiter = new spaceObj({texture: "imgs/texture_jupiter.jpg", size: EARTH_SIZE * 12.5, x: ASTRO_ONE*2,
+		orbitRotationSpeed: EARTH_YEAR * 0.08});
+	OBJECTS['jupiter'] = jupiter;
+	SCENE.add(OBJECTS['jupiter'].getMesh());
+
+	var saturn = new spaceObj({texture: "imgs/texture_saturn.jpg", size: EARTH_SIZE * 10, x: ASTRO_ONE*3,
+		orbitRotationSpeed: EARTH_YEAR * 0.03});
+	OBJECTS['saturn'] = saturn;
+	SCENE.add(OBJECTS['saturn'].getMesh());
 };
 
 function loop() {
 	requestAnimationFrame(loop);
-	OBJECTS['sun'].animate();
+
+	OBJECTS['sun'].selfRotating();
+
+	OBJECTS['mercury'].selfRotating();
+	OBJECTS['mercury'].orbitRotating();
+
+	OBJECTS['venus'].selfRotating();
+	OBJECTS['venus'].orbitRotating();
+
+	OBJECTS['earth'].selfRotating();
+	OBJECTS['earth'].orbitRotating();
+
+	OBJECTS['mars'].selfRotating();
+	OBJECTS['mars'].orbitRotating();
+
+	OBJECTS['jupiter'].selfRotating();
+	OBJECTS['jupiter'].orbitRotating();
+
+	OBJECTS['saturn'].selfRotating();
+	OBJECTS['saturn'].orbitRotating();
+
 	RENDER.render(SCENE, CAMERA);
 };
--- scripts/spaceObj.js
+++ scripts/spaceObj.js
@@ -1,13 +1,25 @@
 function spaceObj(props) {
+    // Mesh geometry and textures
     this.texture = props['texture'];
     this.size = props['size'];
     var geom = new THREE.SphereGeometry(this.size, 32, 32);
     var mat = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load(this.texture)});
     this.mesh = new THREE.Mesh(geom, mat);
 
+    // Begining position
     this.mesh.position.x = props['x'] || 0;
     this.mesh.position.y = props['y'] || 0;
     this.mesh.position.z = props['z'] || 0;
+
+    // Orbits and rotating
+    this.speedOfSelfRotating = props['selfRotatingSpeed'] || 0.001;
+    this.orbitAngle = 0;
+    var dist = Math.sqrt(this.mesh.position.x*this.mesh.position.x
+        + this.mesh.position.y*this.mesh.position.y
+        + this.mesh.position.z + this.mesh.position.z);
+    this.distanceFromSun = props['distanceFromSun'] || dist;
+    this.orbitRotationSpeed = props['orbitRotationSpeed'] || 0.0007; // CHANGE CAREFULLY!!!
+    
 };
 
 spaceObj.prototype.getMesh = function() {
@@ -18,11 +30,13 @@ spaceObj.prototype.getTexture = function () {
     return this.texture;
 }
 
-spaceObj.prototype.animate = function () {
-    /*
-    this.mesh.position.x = this.mesh.position.x * Math.cos((180/Math.PI)*10) -
-        this.mesh.position.y * Math.sin((180/Math.PI)*10);
-    this.mesh.position.y = this.mesh.position.y * Math.cos((180/Math.PI)*10) +
-        this.mesh.position.y * Math.sin((180/Math.PI)*10);
-        */
+spaceObj.prototype.selfRotating = function () {
+    this.mesh.rotation.y += this.speedOfSelfRotating;
+    this.mesh.rotation.z += this.speedOfSelfRotating;
+}
+
+spaceObj.prototype.orbitRotating = function () {
+    this.mesh.position.z = -Math.sin(this.orbitAngle*0.1)*this.distanceFromSun; // TODO maybe change to real orbits x and y
+    this.mesh.position.x = Math.cos(this.orbitAngle*0.1)*this.distanceFromSun;
+    this.orbitAngle += 180/Math.PI*this.orbitRotationSpeed;
 }
